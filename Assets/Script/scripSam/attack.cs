71c71c3fe3c4b111b950b8fd8dfd82baa7dab8a1
samuel1830803 <1830803@etu.cchic.ca>
2023-01-11 21:37:14 -0500

    samuel 21:37-11
    123

--- Assets/Script/scripSam/attack.cs
+++ Assets/Script/scripSam/attack.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class attack : MonoBehaviour
 {
-        public Transform Target;
-        public float Speed= 1f;
-        private Coroutine LookCorutine;
+     
+       
     // Start is called before the first frame update
     void Start()
     {
@@ -21,25 +20,6 @@ public class attack : MonoBehaviour
 
     }
 
-    public void  StartRotating()
-    {
-        if(LookCorutine!=null){
-            StopCoroutine(LookCorutine);
-        } 
-        LookCorutine=StartCorutine(lookAt());
-    }
-    private IEnumerator lookAt()
-    {
-        Quaternion lookRotation= Quaternion.lookRotation(target.position-transform. position);
-
-        float time=0;
-        while(time<1)
-        {
-            transform.rotation=Quaternion.Slerp(transform.rotation, lookRotation,time);
-        }
-        time +=Time.fixedDeltaTime* Speed;
-
-        yield return null;
-    }
+    
 
 }
