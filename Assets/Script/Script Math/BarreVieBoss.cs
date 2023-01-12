f8a9489735f94a77f81ccd10a7c05d2a195ba594
MathieuDionne2032450 <2032450@etu.cchic.ca>
2023-01-12 16:45:54 -0500

    Math

--- Assets/Script/Script Math/BarreVieBoss.cs
+++ Assets/Script/Script Math/BarreVieBoss.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BarreVieBoss : MonoBehaviour
 {
+    [SerializeField]Enemie boss;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,20 @@ public class BarreVieBoss : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(boss);
+        if (boss != null)
+        {
+            
+        }
+    }
+
+    private void afficherVie()
     {
         
     }
