028acc2b8ce15613e6818e1d200a23ef923b6483
Francis Blais <2031420@etu.cchic.ca>
2023-01-12 10:27:03 -0500

    10:26-12
    Script de projectile crée il reste qu'a mettre le bon sprite et mettre sa dans les préfab

---
+++ Assets/Script/Projectile/ShootProjectile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;//crée un objet vide et le placer où le projectile doit partir puis le glisser dans la partie du bulletPos 
+
+    private float timer;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+        if(distance <4)
+        {
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
+                timer = 0;
+                Shoot();
+            }
+        }
+
+       
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+
+
+}
