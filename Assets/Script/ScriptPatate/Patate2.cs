2cce92fd459b67a979f48786156d852d5c812b80
Francis Blais <2031420@etu.cchic.ca>
2023-01-13 00:37:50 -0500

    00:37-13
    Début boss patate bien avancé a finaliser

--- Assets/Script/ScriptPatate/Patate2.cs
+++ Assets/Script/ScriptPatate/Patate2.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class Patate2 : MonoBehaviour
+public class Patate2 : Enemie
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject redLane;
+
+
+    private bool isRolling;
+    private Vector3 stoper;
+
+
+    private void Awake()
     {
-        
+        speed = 0.5f;
+        hpMax = 8;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        canBeAttacked = true;
+        hpActu = hpMax;
+        isRolling = false;
+        redLane = transform.GetChild(0).gameObject;
+        stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        redLane.SetActive(false);
+
+
+    }
+
+
+
+    private void Update()
+    {
+        if (isRolling)
+        {
+            redLane.SetActive(false);
+            transform.position = Vector2.MoveTowards(transform.position, stoper, speed * 5 * Time.deltaTime);//a prendre le stopper
+            if (Vector2.Distance(transform.position, stoper) <= 0.01f)
+            {
+                redLane.SetActive(true);
+                isRolling = false;
+                StartCoroutine(WaitToShoot());
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            redLane.SetActive(true);
+            StartCoroutine(WaitToShoot());
+            gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+
+
+    IEnumerator WaitToShoot()
+    {
+        yield return new WaitForSeconds(2);
+        stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        isRolling = true;
     }
 }
