4a10573a5a0bbb946e401eebf931218ec075711b
Francis Blais <2031420@etu.cchic.ca>
2023-01-12 23:59:28 -0500

    23:59
    patate enfin fini tabarnak

--- Assets/Script/ScriptPatate/Patate1.cs
+++ Assets/Script/ScriptPatate/Patate1.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Patate1 : Enemie
 {
-    public GameObject redLane;
-    public Transform lanePos;//crée un objet vide et le placer où le projectile doit partir puis le glisser dans la partie du bulletPos 
-    private Rigidbody2D rb;
-    private float timer1;
-    private GameObject player;
+    private GameObject redLane;
+    
+    
     private bool isRolling;
-    public Transform stop;
-    // Start is called before the first frame update
+    private Vector3 stoper;
+    
+
     private void Awake()
     {
         speed = 0.5f;
@@ -24,30 +24,46 @@ public class Patate1 : Enemie
         canBeAttacked = true;
         hpActu = hpMax;
         isRolling = false;
+        redLane = transform.GetChild(0).gameObject;
+        stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        redLane.SetActive(false);
+
         
-        // RefreshUI();peut être
     }
 
 
-   private void OnTriggerEnter2D(Collider2D collision)
-   {
-        ShootPatate();
-   }
+  
     private void Update()
     {
-        if(isRolling)
+         if(isRolling)
+         {
+            redLane.SetActive(false);
+            transform.position = Vector2.MoveTowards(transform.position, stoper, speed * 5 * Time.deltaTime);//a prendre le stopper
+            if(Vector2.Distance(transform.position,stoper)<=0.01f)
+            {
+                redLane.SetActive(true);
+                isRolling = false;
+                StartCoroutine(WaitToShoot());
+            }
+         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Player")
         {
-            transform.position = Vector2.MoveTowards(transform.position, stop.position, speed* 5 * Time.deltaTime);
+            redLane.SetActive(true);
+            StartCoroutine(WaitToShoot());
+            gameObject.GetComponent<Collider2D>().enabled = false;
         }
     }
-    private void ShootPatate()
+
+
+    
+    IEnumerator WaitToShoot()
     {
-        Instantiate(redLane,lanePos.position, Quaternion.identity);
-        if(timer1>2)
-        {
-            Destroy(redLane);
-            isRolling = true;
-        }
+        yield return new WaitForSeconds(2);
+        stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        isRolling = true;
     }
 
 }
--- Assets/Script/ScriptPatate/Redlane1.cs
+++ Assets/Script/ScriptPatate/Redlane1.cs
@@ -6,23 +6,24 @@ public class Redlane1 : MonoBehaviour
 {
     public float speed = 100f;
     private Transform target;
-    private float timer;
     
 
     void Start()
     {
        
-
+    }
+    public Vector3 GetStopper()
+    {
+        return transform.GetChild(0).GetChild(0).position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         Vector2 direction = target.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 
