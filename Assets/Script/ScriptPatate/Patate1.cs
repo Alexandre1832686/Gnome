76721e74c1f96b694769c68ed91b5b7aa329c098
Francis Blais <2031420@etu.cchic.ca>
2023-01-13 05:42:26 -0500

    5:42-13
    TRAVAILLEr sur game over

--- Assets/Script/ScriptPatate/Patate1.cs
+++ Assets/Script/ScriptPatate/Patate1.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Patate1 : Enemie
 {
     private GameObject redLane;
-    
-    
+    Gnome gnome;
+    GameObject perso;
     private bool isRolling;
     private Vector3 stoper;
     
@@ -26,20 +26,21 @@ public class Patate1 : Enemie
         isRolling = false;
         redLane = transform.GetChild(0).gameObject;
         stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        perso = GameObject.Find("Perso");
+        gnome = perso.GetComponent<Gnome>();
         redLane.SetActive(false);
-
-        
+        StartCoroutine(attack());
     }
 
 
-  
+
     private void Update()
     {
          if(isRolling)
          {
             redLane.SetActive(false);
             transform.position = Vector2.MoveTowards(transform.position, stoper, speed * 5 * Time.deltaTime);//a prendre le stopper
-            if(Vector2.Distance(transform.position,stoper)<=0.01f)
+            if (Vector2.Distance(transform.position,stoper)<=0.01f)
             {
                 redLane.SetActive(true);
                 isRolling = false;
@@ -63,7 +64,17 @@ public class Patate1 : Enemie
     {
         yield return new WaitForSeconds(2);
         stoper = redLane.GetComponent<Redlane1>().GetStopper();
+        
         isRolling = true;
+    }
+    IEnumerator attack()
+    {
+        yield return new WaitForSeconds(1);
+        if (Vector2.Distance(transform.position, perso.transform.position) <= 0.3)
+        {
+            gnome.retirerVie();
+        }
+        StartCoroutine(attack());
     }
 
 }
