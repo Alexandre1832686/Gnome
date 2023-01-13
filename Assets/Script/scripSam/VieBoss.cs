7082c081212b29821f6c627cd3aebe8b61a067f5
samuel1830803 <1830803@etu.cchic.ca>
2023-01-13 00:53:29 -0500

    samuel 00:53 13
    assd

--- Assets/Script/scripSam/VieBoss.cs
+++ Assets/Script/scripSam/VieBoss.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class VieBoss : MonoBehaviour
 {
     [SerializeField] GameObject Canvas;
+    GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boss=GameObject.Find("Doight");
     }
 
     // Update is called once per frame
@@ -20,7 +21,8 @@ public class VieBoss : MonoBehaviour
     private void onTriggerEnter2D(Collider2D collision){
         if(collision.tag == "Player")
         {
-            
+
+            boss.GetComponent<BossFinal>().StartFight();
             Canvas.SetActive(true);
             Destroy(gameObject);
         }
